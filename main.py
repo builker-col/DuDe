640aaa5de40652787c85397cd52927ae28aec3fe
zer0 <erik172dev@gmail.com>
2024-05-13 22:19:07 -0500

    Refactor find_duplicates_tqdm method in DuDe class

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import json
 from colorama import Fore, Style
 
 dude = DuDe('data/')
-dude.find_duplicates_tqdm
+dude.find_duplicates_tqdm()
 
 duplicates = dude.get_duplicates()
 
 for key, value in duplicates.items():
     if len(value) > 1:
-        print(f'{Fore.RED}{key} has {len(value)} duplicates: {value}{Style.RESET_ALL}')
+        print(f'{Fore.LIGHTRED_EX}{key} has {len(value)} duplicates: {value}{Style.RESET_ALL} \n')
     else:
-        print(f'{Fore.GREEN}{value} is duplicated by {key}{Style.RESET_ALL}')
+        print(f'{Fore.LIGHTRED_EX}{value[0]} is duplicated by {key}{Style.RESET_ALL} \n')
 
 hash_map = dude.get_hash_map()
 
