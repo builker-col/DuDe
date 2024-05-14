2b3aefa43392ab111683d90e5023be8e94bc846d
zer0 <erik172dev@gmail.com>
2024-05-13 21:48:48 -0500

    chore: Add .gitignore for duplicates.json and hash_map.json

---
+++ main.py
@@ -0,0 +1,25 @@
+from resources import DuDe
+import colorama
+import json
+from colorama import Fore, Style
+
+dude = DuDe('data/')
+dude.find_duplicates()
+
+duplicates = dude.get_duplicates()
+
+for key, value in duplicates.items():
+    if len(value) > 1:
+        print(f'{Fore.RED}{key} has {len(value)} duplicates: {value}{Style.RESET_ALL}')
+    else:
+        print(f'{Fore.GREEN}{value} is duplicated by {key}{Style.RESET_ALL}')
+
+hash_map = dude.get_hash_map()
+
+with open('hash_map.json', 'w') as f:
+    json.dump(hash_map, f)
+
+with open('duplicates.json', 'w') as f:
+    json.dump(duplicates, f)
+
+print('Data written to hash_map.json and duplicates.json')
